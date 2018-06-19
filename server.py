c276f963cded05963b0442f363b73a3700ce4e0e
pietroborrello <pietro.borrello95@gmail.com>
2018-06-19 03:05:06 +0200

    removed duplicates points in mds

--- server.py
+++ server.py
@@ -19,32 +19,37 @@ def index():
 
 @app.route("/mds", methods=['POST','GET'])
 def mds():
+    DISSIMILARITY = 'modified_regs'
     with open('./files/ls.json') as f:
         data_file = json.load(f)
 
     data_filter = filter(lambda d: d['type'] == 'LoadConst' and d['params'] == 'edi', data_file)
     _data = []
 
+    added = set()
     for d in data_filter:
-        _data.append(d)
+        dissimilar = frozenset(d[DISSIMILARITY])
+        if dissimilar not in added:
+            _data.append(d)
+            added.add(dissimilar)
 
     data = pd.io.json.read_json(json.dumps(_data))
     dissM=np.zeros((len(data),len(data))) #creates a zeros dissM
 
     for i in range(len(data)):
         for j in range (len(data)):
-            a1 = set(data['modified_regs'][i])
-            b1 = set(data['modified_regs'][j])
-            a2 = set(data['mem'][i][0])
-            b2 = set(data['mem'][j][0])
-            dissM[i][j]= len((a1 | b1) - (a1 & b1))# + len((a2 | b2) - (a2 & b2))
+            a = set(data[DISSIMILARITY][i])
+            b = set(data[DISSIMILARITY][j])
+            #a = set(data['mem'][i][0])
+            #b = set(data['mem'][j][0])
+            dissM[i][j]= len((a | b) - (a & b))
     
     mds = manifold.MDS(n_components=2, max_iter=3000, eps=1e-9,
                    dissimilarity="precomputed")
     pos = mds.fit(dissM).embedding_
 
     res = {'X': [], 'labels': []}
-    for label, x, y in zip(data['modified_regs'], pos[:, 0], pos[:, 1]):
+    for label, x, y in zip(data[DISSIMILARITY], pos[:, 0], pos[:, 1]):
         res['X'].append({"x": x, "y": y})
         res['labels'].append(label)
 
